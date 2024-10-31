b91236dee95cb631bab94f189b0ad74d57f2352c
MH-Rohan-JU <rohan70302@gmail.com>
2024-10-31 22:12:19 +0600

    DSA Notes

---
+++ 6_SelectionSort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define endl "\n"
+#define ll long long
+
+int main()
+{
+    int n, i, j;
+    cout << "Enter the total number of array: ";
+    cin >> n;
+    int arr[n];
+
+    cout << "\nEnter " << n << " elements: ";
+    for (i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // Selection Sort
+    for (i = n - 1; i >= 1; i--)
+    {
+        int maxEl = arr[0];
+        int maxInd = 0;
+        for (j = 0; j <= i; j++)
+        {
+            if (arr[j] > maxEl)
+            {
+                maxEl = arr[j];
+                maxInd = j;
+            }
+        }
+        swap(arr[maxInd], arr[--j]);
+    }
+
+    cout << "\nAfter applying selection sort, the sorted array:\n";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    return 0;
+}
