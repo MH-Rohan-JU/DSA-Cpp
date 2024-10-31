65b42607c2f0a22b1957c0bfbd48b294dd64fec7
MH-Rohan-JU <rohan70302@gmail.com>
2024-10-31 21:30:17 +0600

    DSA Notes

---
+++ 5_AckermannFucntion.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int Ackermann(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (n == 0)
+        return Ackermann(m - 1, 1);
+    else
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+}
+int main()
+{
+    int n, m;
+    cout << "Enter 2 non-negative integers: ";
+    cin >> n >> m;
+    cout << "Ackermann(" << n << ", " << m << ") = " << Ackermann(n, m);
+    return 0;
+}
