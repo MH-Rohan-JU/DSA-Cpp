ba109924ef416e29840b1ae7f9c03a1deb80ecef
MH-Rohan-JU <rohan70302@gmail.com>
2024-11-07 10:17:33 +0600

    DSA Notes

---
+++ 8_InsertionSort.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ll long long
+
+void insertionSort(int arr[], int s)
+{
+    int i, j;
+    for (i = 1; i < s; i++)
+    {
+        int temp = arr[i];
+        j = i - 1;
+        while (arr[j] > temp && j > -1)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
+    return;
+}
+int main()
+{
+    int size = 8;
+    int arr[size] = {56, 71, 28, 98, 12, 44, 64, 35};
+
+    cout << "The unsorted Array:\n";
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    insertionSort(arr, size); // Insertion Sort func call
+
+    cout << "\nAfter applying insertion sort" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
